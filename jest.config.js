9acc8731e4e260bddacd9cd201b660262a79f66e
= <=>
2023-12-23 17:17:41 +0900

    complete code

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+/** @type {import('ts-jest/dist/types').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  setupFilesAfterEnv: ['<rootDir>/src/setupFilesAfterEnv.ts'],
   modulePathIgnorePatterns: ['<rootDir>/dist/'],
 };
---
+++ src/api/todos/todos.routes.ts
@@ -0,0 +1,40 @@
+import { Router } from 'express'
+import * as TodoHandlers from './todos.handlers'
+import { Todo } from './todos.model'
+import { validateRequest } from '../../middlewares'
+import { ParamsWithId } from '../../interfaces/paramsWithId'
+
+const router = Router()
+
+router.get('/', TodoHandlers.findAll)
+router.get(
+  '/:id',
+  validateRequest({
+    params: ParamsWithId
+  }),
+  TodoHandlers.findOne
+)
+router.post(
+  '/',
+  validateRequest({
+    body: Todo
+  }),
+  TodoHandlers.createOne
+)
+router.put(
+  '/:id',
+  validateRequest({
+    params: ParamsWithId,
+    body: Todo
+  }),
+  TodoHandlers.updateOne
+)
+router.delete(
+  '/:id',
+  validateRequest({
+    params: ParamsWithId
+  }),
+  TodoHandlers.deleteOne
+)
+
+export default router
